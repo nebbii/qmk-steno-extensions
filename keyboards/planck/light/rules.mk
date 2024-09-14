b53bd13c70e569972aa835ed25917d967df7dbdd
ben <ben.wolthuis@gmail.com>
2024-09-14 09:40:33 +0200

    Update rules.mk

--- keyboards/planck/light/rules.mk
+++ keyboards/planck/light/rules.mk
@@ -9,16 +9,15 @@ BOOTLOADER = atmel-dfu
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes         # Console for debug
-COMMAND_ENABLE = no        # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
-MIDI_ENABLE = yes           # MIDI support
 AUDIO_ENABLE = yes           # Audio output
-RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = IS31FL3731
 
+
 LAYOUTS = ortho_4x12 planck_mit
 LAYOUTS_HAS_RGB = no
+
+STENO_ENABLE = yes		# Stenography keys
+STENO_PROTOCOL = all
