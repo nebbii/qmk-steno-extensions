be5cd2bf5e9b8ff416eaf00bef79bfe9c2a1c135
ben <ben.wolthuis@gmail.com>
2024-09-14 09:03:07 +0200

    Update keymap.c

--- keyboards/planck/keymaps/default/keymap.c
+++ keyboards/planck/keymaps/default/keymap.c
@@ -15,16 +15,17 @@
  */
 
 #include QMK_KEYBOARD_H
+#include "keymap_steno.h"
 #include "muse.h"
 
 
 enum planck_layers {
-  _PLOVER,
   _QWERTY,
   _COLEMAK,
   _DVORAK,
   _LOWER,
   _RAISE,
+  _PLOVER,
   _ADJUST
 };
 
@@ -135,20 +136,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Plover layer (http://opensteno.org)
  * ,-----------------------------------------------------------------------------------.
- * |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |   S  |   T  |   P  |   H  |   *  |   *  |   F  |   P  |   L  |   T  |   D  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |   S  |   K  |   W  |   R  |   *  |   *  |   R  |   B  |   G  |   S  |   Z  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Exit |      |      |   A  |   O  |             |   E  |   U  |      |      |      |
+ * | RAISE| Ctrl | Alt  | GUI  |LShift|      |      |      | LEFT | DOWN |  UP  | RIGHT|
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | LOWER|      |      |      |   A  |   O  |   E  |   U  |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_PLOVER] = LAYOUT_planck_grid(
     XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
     XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    RAISE,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    LOWER,   XXXXXXX, XXXXXXX, KC_C,    KC_V,    XXXXXXX, XXXXXXX, KC_N,    KC_M,    XXXXXXX, XXXXXXX, XXXXXXX
+    RAISE,   KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
+    LOWER,   XXXXXXX, XXXXXXX, XXXXXXX, KC_C,    KC_V,    KC_N,    KC_M,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 /* Adjust (Lower + Raise)
